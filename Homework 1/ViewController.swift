250c63b2fc90bc52ea5d0d605d3235033cb79ffe
Talon DeRocco <t.derocco@setonhill.edu>
2020-02-03 23:35:53 -0500

    ready for review

---
+++ Homework 1/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  Homework 1
+//
+//  Created by Talon DeRocco on 2/3/20.
+//  Copyright © 2020 Talon DeRocco. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
